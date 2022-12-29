01f0e08839eeffd0585f63634eea535379ac81a4
SOLIDOR\emper <emperorfluffyxd@gmail.com>
2022-12-29 15:04:39 -0800

    +Adjusted tooltip update logic

--- src/helpers/svelte-actions/Tooltip.js
+++ src/helpers/svelte-actions/Tooltip.js
@@ -1,30 +1,44 @@
 import tippy from 'tippy.js';
 import 'tippy.js/dist/tippy.css';
 
-export default function tooltip(node, params = {}) {
-    if (params?.content) {
-      const content = params.content;
-      const allowHTML = true;
-      const delay = [1000,250];
-      const duration = [400,250];
-  
-      // Support any of the Tippy props by forwarding all "params":
-      // https://atomiks.github.io/tippyjs/v6/all-props/
-      const tooltip = tippy(node, { content, allowHTML, duration, delay, ...params });
-    
-      return {
-        // If the props change, let's update the Tippy instance:
-        update: (newParams) => {
-          console.log(content);
-          tooltip.setProps({ content, allowHTML, duration, delay, ...newParams })
-        },
-    
-        // Clean up the Tippy instance on unmount:
-        destroy: () => tooltip.destroy(),
-      };
-  
-    }
+export default function tooltip(node, params) {
+   let tippy = params?.content ? initializeTippy(node, params) : false;
 
-    return;
+   return {
+      update: (newParams) => {
+         if (newParams?.content) {
 
+            if (tippy) {
+               const content = newParams.content;
+               const allowHTML = true;
+               const delay = [1000, 250];
+               const duration = [400, 250];
+               tippy.setProps({ content, allowHTML, duration, delay });
+            }
+
+            else {
+               tippy = initializeTippy(node, params);
+            }
+         }
+
+         else if (this.tippy) {
+            tippy.destroy();
+            tippy = false;
+         }
+      },
+
+      destroy: {
+         if(tippy) {
+            tippy.destroy();
+         }
+      }
+   };
+}
+
+function initializeTippy(node, params) {
+   const content = params.content;
+   const allowHTML = true;
+   const delay = [1000, 250];
+   const duration = [400, 250];
+   return tippy(node, { content, allowHTML, duration, delay });
 }
