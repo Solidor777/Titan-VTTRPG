b8254b19d7c3608fb05407b82721d64181be949f
SOLIDOR\emper <emperorfluffyxd@gmail.com>
2026-01-30 00:26:04 -0800

    Fixed checkbox styling for consistency

--- src/helpers/svelte-components/input/CheckboxInput.svelte
+++ src/helpers/svelte-components/input/CheckboxInput.svelte
@@ -1,6 +1,5 @@
 <script>
    import { createEventDispatcher } from 'svelte';
-   import { CHECKED_ICON, UNCHECKED_ICON } from '~/system/Icons.js';
    import preventDefault from '~/helpers/svelte-actions/PreventDefault.js';
    import tooltipAction from '~/helpers/svelte-actions/TooltipAction.js';
 
@@ -28,37 +27,22 @@
    on:click={onClick}
    on:mousedown={preventDefault}
    use:tooltipAction={tooltip}>
-   <i class={value === true ? CHECKED_ICON : UNCHECKED_ICON}/>
+   {#if value === true}
+      <i class="fas fa-check"/>
+   {/if}
+
 </button>
 
 <style lang="scss">
    button {
-      background: var(--titan-input-font-color);
-      border-color: var(--titan-input-border-color);
-      border-style: var(--titan-input-border-style);
-      border-width: var(--titan-input-border-width);
-      box-sizing: border-box;
-      color: var(--titan-input-background);
-      font-size: var(--titan-input-height);
+      @include input;
+
+      background: radial-gradient(var(--titan-input-background));
       height: var(--titan-input-height);
       margin: 0;
       min-height: var(--titan-input-height);
-      min-width: var(--titan-input-min-height);
+      min-width: var(--titan-input-height);
       padding: 0;
-      width: var(--titan-input-min-height);
-
-      &:disabled {
-         --titan-input-background: var(--titan-input-disabled-background);
-         --titan-input-border-color: var(--titan-input-disabled-border-color);
-         --titan-input-font-color: var(--titan-input-disabled-font-color);
-      }
-
-      &:not(:disabled) {
-         &:hover {
-            --titan-input-border-color: var(--titan-input-hover-border-color);
-            --titan-input-font-color: var(--titan-input-hover-font-color);
-            --titan-input-background: var(--titan-input-hover-background);
-         }
-      }
+      width: var(--titan-input-height);
    }
 </style>
