c9f78023412d33ca461a471ffa448457de84d56d
SOLIDOR\emper <emperorfluffyxd@gmail.com>
2026-05-29 14:20:11 -0700

    refactor(svelte5): convert slotted tags to children snippets

--- src/helpers/svelte-components/tag/ResistanceTag.svelte
+++ src/helpers/svelte-components/tag/ResistanceTag.svelte
@@ -1,17 +1,21 @@
 <script>
    import tooltipAction from '~/helpers/svelte-actions/TooltipAction.js';
 
-   /** @type {string} The Resistance to associate with the tag. */
-   export let resistance = void 0;
+   /**
+    * @typedef {object} ResistanceTagProps
+    * @property {string} [resistance] - The Resistance to associate with the tag.
+    * @property {string | TooltipAction} [tooltip] - The Tooltip to display for this element, if any.
+    * @property {import('svelte').Snippet} [children] - Default slot content.
+    */
 
-   /** @type {string | TooltipAction} The Tooltip to display for this element, if any. */
-   export let tooltip = void 0;
+   /** @type {ResistanceTagProps} */
+   let { resistance = void 0, tooltip = void 0, children } = $props();
 </script>
 
 <div
    class="tag {resistance}"
    use:tooltipAction={tooltip}>
-   <slot/>
+   {@render children?.()}
 </div>
 
 <style lang="scss">
