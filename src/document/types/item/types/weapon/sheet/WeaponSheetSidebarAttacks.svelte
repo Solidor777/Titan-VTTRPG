29c8e2fa4b292c6f3aa42a014473c391be389e77
SOLIDOR\emper <emperorfluffyxd@gmail.com>
2026-06-05 22:47:16 -0700

    refactor(weapon-sheet): render sidebar attack stats through shared AttackTags

--- src/document/types/item/types/weapon/sheet/WeaponSheetSidebarAttacks.svelte
+++ src/document/types/item/types/weapon/sheet/WeaponSheetSidebarAttacks.svelte
@@ -2,27 +2,20 @@
    import { getContext } from 'svelte';
    import { slide } from 'svelte/transition';
    import localize from '~/helpers/utility-functions/Localize.js';
-   import { ATTACK_TRAIT_DESCRIPTIONS } from '~/document/types/item/types/weapon/AttackTraits.js';
-   import StatTag from '~/helpers/svelte-components/tag/StatTag.svelte';
    import IconButton from '~/helpers/svelte-components/button/IconButton.svelte';
-   import IconStatTag from '~/helpers/svelte-components/tag/IconStatTag.svelte';
+   import AttackTags from '~/document/types/item/types/weapon/components/AttackTags.svelte';
    import {
       COLLAPSED_ICON,
-      DAMAGE_ICON,
       EXPANDED_ICON,
       MELEE_ICON,
       RANGE_ICON,
    } from '~/system/Icons.js';
-   import Tag from '~/helpers/svelte-components/tag/Tag.svelte';
-   import AttributeCheckTag from '~/helpers/svelte-components/tag/AttributeCheckTag.svelte';
 
    /** @type {object} Reference to the reactive Document store. */
    const document = getContext('document');
 
    /** @type {object} Reference to the Application State store. */
    const appState = getContext('applicationState');
-
-   const traitDescriptions = ATTACK_TRAIT_DESCRIPTIONS;
 </script>
 
 <ol>
@@ -68,68 +61,7 @@
 
          {#if $appState.sidebar.attacks.isExpanded[idx]}
             <div class="stats" transition:slide|local>
-               <!--Attack Type-->
-               <div class="stat">
-                  <Tag>{localize(attack.type)}</Tag>
-               </div>
-
-               <!--Range-->
-               {#if attack.range !== 1}
-                  <div class="stat" transition:slide|local>
-                     <IconStatTag
-                        label={localize('range')}
-                        value={attack.range}
-                        icon={RANGE_ICON}
-                     />
-                  </div>
-               {/if}
-
-               <!--Damage-->
-               <div class="stat">
-                  <IconStatTag
-                     icon={DAMAGE_ICON}
-                     label={localize('damage')}
-                     value={`${attack.damage}${
-                        attack.plusExtraSuccessDamage
-                           ? ` + ${localize('extraSuccesses.short')}`
-                           : ''
-                     }`}
-                  />
-               </div>
-
-               <!--Skill & Attribute-->
-               <div class="stat">
-                  <AttributeCheckTag
-                     attribute={attack.attribute}
-                     skill={attack.skill}
-                  />
-               </div>
-
-               <!--Normal Traits-->
-               {#each attack.trait as trait (trait.name)}
-                  <div class="stat" transition:slide|local>
-                     {#if typeof (trait.value) === 'number'}
-                        <!--Number Trait-->
-                        <StatTag
-                           tooltip={traitDescriptions[trait.name]}
-                           label={localize(trait.name)}
-                           value={trait.value}
-                        />
-                     {:else}
-                        <!--Bool Trait-->
-                        <Tag tooltip={traitDescriptions[trait.name]}>{localize(trait.name)}</Tag>
-                     {/if}
-                  </div>
-               {/each}
-
-               <!--Custom Traits-->
-               {#each attack.customTrait as trait (trait.uuid)}
-                  <div class="stat" transition:slide|local>
-                     <Tag tooltip={{ text: trait.description, localize: false }}>
-                        {trait.name}
-                     </Tag>
-                  </div>
-               {/each}
+               <AttackTags {idx}/>
             </div>
          {/if}
       </li>
@@ -197,15 +129,9 @@
             @include flex-group-center;
             @include border-bottom-sides;
             @include panel-3;
-            @include font-size-small;
 
             width: calc(100% - var(--titan-spacing-large));
-            flex-wrap: wrap;
             padding: 0 var(--titan-spacing-standard) var(--titan-spacing-large) var(--titan-spacing-standard);
-
-            .stat {
-               margin: var(--titan-spacing-large) var(--titan-spacing-standard) 0 var(--titan-spacing-standard);
-            }
          }
       }
    }
